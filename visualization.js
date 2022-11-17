ac28f9d154b5dd6933b08761d834efb4d44f50c8
JBarr42 <92589400+JBarr42@users.noreply.github.com>
2022-11-17 09:51:30 -0500

    Update visualization.js

--- visualization.js
+++ visualization.js
@@ -1,1 +1,43 @@
 
+d3.csv("stream-data.csv").then(
+    function(dataset){
+        
+        console.log(dataset)
+
+        var dimensions = {
+            width: 1400,
+            height: 800,
+            margin:{
+                top: 10,
+                bottom: 50,
+                right: 10,
+                left: 50
+            }
+        }
+
+        var svg = d3.select("#stackedbarchart")
+                    .style("width", dimensions.width)
+                    .style("height", dimensions.height)
+                    
+        console.log()
+
+        var xScale = d3.scaleBand()
+                       .domain(d3.map(dataset, d => +d.year))
+                       .range([dimensions.margin.left ,dimensions.width - dimensions.margin.right])
+                       .padding([0.2])
+
+        var keys = dataset.columns.slice(1)
+        console.log(keys)
+
+        var maxSum = d3.max(dataset, function(d){
+            var sumName = 0
+            for(var i=0; i<keys.length; i++){
+                sumName = sumName + parseInt(d[keys[i]])
+            }
+
+            return sumName
+        })
+
+
+    }
+)
