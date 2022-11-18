3e3b7912d3775c01e356dc185be3e9c154dcc6fb
Janelle333 <116041649+Janelle333@users.noreply.github.com>
2022-11-17 22:11:15 -0500

    Update visualization.js

--- visualization.js
+++ visualization.js
@@ -2,7 +2,7 @@
 d3.csv("4030-visualization-data.csv").then(
     function(dataset){
         
-        console.log(dataset)
+        //console.log(dataset)
 
         var dimensions = {
             width: 700,
@@ -19,29 +19,43 @@ d3.csv("4030-visualization-data.csv").then(
                     .style("width", dimensions.width)
                     .style("height", dimensions.height)
                     
-        console.log()
 
-        var xScale = d3.scaleBand()
-                       .domain(d3.map(dataset, d => +d.year))
+        var xScale = d3.scaleBand()//arcs
+                       //.domain(d3.map(dataset, d => +d.year))
+                       .domain(dataset.map(function(d){return d.Arc}))
                        .range([dimensions.margin.left ,dimensions.width - dimensions.margin.right])
                        .padding([0.2])
 
-        var keys = dataset.columns.slice(1)
+        var keys = dataset.columns.slice(1,2)//remove first column of csv
         console.log(keys)
 
-        var maxSum = d3.max(dataset, function(d){
+        var num = d3.count(dataset, d => d.Filler);
+        //console.log(num);
+
+       /* var maxSum = d3.max(dataset, function(d){
         var sumName = 0
             for(var i=0; i<keys.length; i++){
                 sumName = sumName + parseInt(d[keys[i]])
             }
 
             return sumName
-        })
+        })*/
 
         var yScale = d3.scaleLinear()
-                    .domain([0, maxSum])
+                    .domain([0, num])
                     .range([dimensions.height-dimensions.margin.bottom, dimensions.margin.top])
 
+        
+                    var xAxisGen = d3.axisBottom().scale(xScale)
+         var xAxis = svg.append("g")
+                        .call(xAxisGen)
+                        .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom}px)`)
+
+         var yAxisGen = d3.axisLeft().scale(yScale)
+         var yAxis = svg.append("g")
+                         .call(yAxisGen)
+                         .style("transform", `translateX(${dimensions.margin.left}px)`)
+        
         var colorScale = d3.scaleOrdinal()
                         .domain(keys)
                         .range(["#b3e2cd","#fdcdac","#cbd5e8"])
