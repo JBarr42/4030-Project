1763b3dc33e7a44426417e259c8d3ddb1f3396c8
JBarr42 <92589400+JBarr42@users.noreply.github.com>
2022-12-15 16:31:40 -0500

    Update visualization.js

--- visualization.js
+++ visualization.js
@@ -58,118 +58,6 @@ d3.csv("4030-visualization-data.csv").then(
                         .range(["#F4D03F", "#3498DB"])
     
     
-//BARCHART
-        var svg = d3.select("#barchart")
-                  .style("width", dimensions.width)
-                  .style("height", dimensions.height)
-//add options to the button
-Arcs = ['Arc 1', 'Arc 2', 'Arc 3', 'Arc 4', 'Arc 5']
-var options = Arcs
-d3.select("#selectButton")
-                .selectAll('myOptions')
-                .data(options)
-                .enter()
-                .append('option')
-                    .text(function (d) { return d; }) // text showed in the menu
-                    .attr("value", function (d) { return d; }) // corresponding value returned by the button
-    
-    var tickLabels = ['Filler','Canon']
-    var xScale = d3.scaleBand()
-                    .domain(tickLabels)
-                    .range([dimensions.margin.left ,dimensions.width - dimensions.margin.right])
-                    .padding(0.4)
-    var yScale = d3.scaleLinear()
-                    .domain([0, d3.max([m1,m2,m3,m4,m5,m6,m7,m8,m9,m10])])
-                    .range([dimensions.height-dimensions.margin.bottom, dimensions.margin.top])
-    //adds axes to the chart
-    var xAxisGen = d3.axisBottom().scale(xScale)
-    var xAxis = svg.append("g")
-                    .call(xAxisGen)
-                    .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom}px)`)
-    var yAxisGen = d3.axisLeft().scale(yScale)
-    var yAxis = svg.append("g")
-                        .call(yAxisGen)
-                        .style("transform", `translateX(${dimensions.margin.left}px)`)
-    var bars = svg.append("g")
-                    .selectAll("g")
-                    .data(Episodes[0])
-                    .enter()
-                    .append("rect")
-                    .attr("fill", (d,i) => {return colorScale1(i)})
-                    .attr("x", (d,i) => {
-                                        if(i == 0){
-                                            return xScale("Filler")
-                                        }
-                                        if(i == 1){
-                                            return xScale("Canon")
-                                        }
-                                    })
-                    .attr("y", d => yScale(d))
-                    .attr("width", d => xScale.bandwidth())
-                    .attr("height", d => yScale(0) - yScale(d))
-    
-     
-                    // A function that update the chart
-    function update(selectedGroup) {
-    // Create new data with the selection
-        if(selectedGroup == 'Arc 1'){
-            var dataFilter = Episodes[0]
-        }
-        if(selectedGroup == 'Arc 2'){
-            var dataFilter = Episodes[1]
-        }
-        if(selectedGroup == 'Arc 3'){
-            var dataFilter = Episodes[2]
-        }
-        if(selectedGroup == 'Arc 4'){
-            var dataFilter = Episodes[3]
-        }
-        if(selectedGroup == 'Arc 5'){
-            var dataFilter = Episodes[4]
-        }
-    console.log(dataFilter)
-    // Give these new data to update line
-    bars
-        .data(dataFilter)
-        .transition()
-        .duration(1000)
-        .attr("fill", (d,i) => {
-            if(selectedGroup == 'Arc 1'){
-                return colorScale1(i)
-            }
-            if(selectedGroup == 'Arc 2'){
-                return colorScale2(i)
-            }
-            if(selectedGroup == 'Arc 3'){
-                return colorScale3(i)
-            }
-            if(selectedGroup == 'Arc 4'){
-                return colorScale4(i)
-            }
-            if(selectedGroup == 'Arc 5'){
-                return colorScale5(i)
-            }
-        })
-        .attr("x", (d,i) => {
-            if(i == 0){
-                return xScale("Filler")
-            }
-            if(i == 1){
-                return xScale("Canon")
-            }
-        })
-        .attr("y", d => yScale(d))
-        .attr("width", d => xScale.bandwidth())
-        .attr("height", d => yScale(0) - yScale(d))
-  }
-  // When the button is changed, run the updateChart function
-  d3.select("#selectButton").on("change", function(d) {
-    // recover the option that has been chosen
-    var selectedOption = d3.select(this).property("value")
-    // run the updateChart function with this selected option
-    update(selectedOption)
- 
-  
                         //LINE GRAPH
 var svg = d3.select("#linegraph")
 .style("width", dimensions.width)
@@ -449,119 +337,119 @@ var yAxis = svg.append("g")
                                           
         console.log(bars)
     })
-//     //BARCHART
-//         var svg = d3.select("#barchart")
-//                   .style("width", dimensions.width)
-//                   .style("height", dimensions.height)
-// //add options to the button
-// Arcs = ['Arc 1', 'Arc 2', 'Arc 3', 'Arc 4', 'Arc 5']
-// var options = Arcs
-// d3.select("#selectButton")
-//                 .selectAll('myOptions')
-//                 .data(options)
-//                 .enter()
-//                 .append('option')
-//                     .text(function (d) { return d; }) // text showed in the menu
-//                     .attr("value", function (d) { return d; }) // corresponding value returned by the button
+    //BARCHART
+        var svg = d3.select("#barchart")
+                  .style("width", dimensions.width)
+                  .style("height", dimensions.height)
+//add options to the button
+Arcs = ['Arc 1', 'Arc 2', 'Arc 3', 'Arc 4', 'Arc 5']
+var options = Arcs
+d3.select("#selectButton")
+                .selectAll('myOptions')
+                .data(options)
+                .enter()
+                .append('option')
+                    .text(function (d) { return d; }) // text showed in the menu
+                    .attr("value", function (d) { return d; }) // corresponding value returned by the button
     
-//     var tickLabels = ['Filler','Canon']
-//     var xScale = d3.scaleBand()
-//                     .domain(tickLabels)
-//                     .range([dimensions.margin.left ,dimensions.width - dimensions.margin.right])
-//                     .padding(0.4)
-//     var yScale = d3.scaleLinear()
-//                     .domain([0, d3.max([m1,m2,m3,m4,m5,m6,m7,m8,m9,m10])])
-//                     .range([dimensions.height-dimensions.margin.bottom, dimensions.margin.top])
-//     //adds axes to the chart
-//     var xAxisGen = d3.axisBottom().scale(xScale)
-//     var xAxis = svg.append("g")
-//                     .call(xAxisGen)
-//                     .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom}px)`)
-//     var yAxisGen = d3.axisLeft().scale(yScale)
-//     var yAxis = svg.append("g")
-//                         .call(yAxisGen)
-//                         .style("transform", `translateX(${dimensions.margin.left}px)`)
-//     var bars = svg.append("g")
-//                     .selectAll("g")
-//                     .data(Episodes[0])
-//                     .enter()
-//                     .append("rect")
-//                     .attr("fill", (d,i) => {return colorScale1(i)})
-//                     .attr("x", (d,i) => {
-//                                         if(i == 0){
-//                                             return xScale("Filler")
-//                                         }
-//                                         if(i == 1){
-//                                             return xScale("Canon")
-//                                         }
-//                                     })
-//                     .attr("y", d => yScale(d))
-//                     .attr("width", d => xScale.bandwidth())
-//                     .attr("height", d => yScale(0) - yScale(d))
+    var tickLabels = ['Filler','Canon']
+    var xScale = d3.scaleBand()
+                    .domain(tickLabels)
+                    .range([dimensions.margin.left ,dimensions.width - dimensions.margin.right])
+                    .padding(0.4)
+    var yScale = d3.scaleLinear()
+                    .domain([0, d3.max([m1,m2,m3,m4,m5,m6,m7,m8,m9,m10])])
+                    .range([dimensions.height-dimensions.margin.bottom, dimensions.margin.top])
+    //adds axes to the chart
+    var xAxisGen = d3.axisBottom().scale(xScale)
+    var xAxis = svg.append("g")
+                    .call(xAxisGen)
+                    .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom}px)`)
+    var yAxisGen = d3.axisLeft().scale(yScale)
+    var yAxis = svg.append("g")
+                        .call(yAxisGen)
+                        .style("transform", `translateX(${dimensions.margin.left}px)`)
+    var bars = svg.append("g")
+                    .selectAll("g")
+                    .data(Episodes[0])
+                    .enter()
+                    .append("rect")
+                    .attr("fill", (d,i) => {return colorScale1(i)})
+                    .attr("x", (d,i) => {
+                                        if(i == 0){
+                                            return xScale("Filler")
+                                        }
+                                        if(i == 1){
+                                            return xScale("Canon")
+                                        }
+                                    })
+                    .attr("y", d => yScale(d))
+                    .attr("width", d => xScale.bandwidth())
+                    .attr("height", d => yScale(0) - yScale(d))
     
      
-//                     // A function that update the chart
-//     function update(selectedGroup) {
-//     // Create new data with the selection
-//         if(selectedGroup == 'Arc 1'){
-//             var dataFilter = Episodes[0]
-//         }
-//         if(selectedGroup == 'Arc 2'){
-//             var dataFilter = Episodes[1]
-//         }
-//         if(selectedGroup == 'Arc 3'){
-//             var dataFilter = Episodes[2]
-//         }
-//         if(selectedGroup == 'Arc 4'){
-//             var dataFilter = Episodes[3]
-//         }
-//         if(selectedGroup == 'Arc 5'){
-//             var dataFilter = Episodes[4]
-//         }
-//     console.log(dataFilter)
-//     // Give these new data to update line
-//     bars
-//         .data(dataFilter)
-//         .transition()
-//         .duration(1000)
-//         .attr("fill", (d,i) => {
-//             if(selectedGroup == 'Arc 1'){
-//                 return colorScale1(i)
-//             }
-//             if(selectedGroup == 'Arc 2'){
-//                 return colorScale2(i)
-//             }
-//             if(selectedGroup == 'Arc 3'){
-//                 return colorScale3(i)
-//             }
-//             if(selectedGroup == 'Arc 4'){
-//                 return colorScale4(i)
-//             }
-//             if(selectedGroup == 'Arc 5'){
-//                 return colorScale5(i)
-//             }
-//         })
-//         .attr("x", (d,i) => {
-//             if(i == 0){
-//                 return xScale("Filler")
-//             }
-//             if(i == 1){
-//                 return xScale("Canon")
-//             }
-//         })
-//         .attr("y", d => yScale(d))
-//         .attr("width", d => xScale.bandwidth())
-//         .attr("height", d => yScale(0) - yScale(d))
-//   }
-//   // When the button is changed, run the updateChart function
-//   d3.select("#selectButton").on("change", function(d) {
-//     // recover the option that has been chosen
-//     var selectedOption = d3.select(this).property("value")
-//     // run the updateChart function with this selected option
-//     update(selectedOption)
-//  // }
-//   //)
-//   }
-//   )
-//     }
-// )
+                    // A function that update the chart
+    function update(selectedGroup) {
+    // Create new data with the selection
+        if(selectedGroup == 'Arc 1'){
+            var dataFilter = Episodes[0]
+        }
+        if(selectedGroup == 'Arc 2'){
+            var dataFilter = Episodes[1]
+        }
+        if(selectedGroup == 'Arc 3'){
+            var dataFilter = Episodes[2]
+        }
+        if(selectedGroup == 'Arc 4'){
+            var dataFilter = Episodes[3]
+        }
+        if(selectedGroup == 'Arc 5'){
+            var dataFilter = Episodes[4]
+        }
+    console.log(dataFilter)
+    // Give these new data to update line
+    bars
+        .data(dataFilter)
+        .transition()
+        .duration(1000)
+        .attr("fill", (d,i) => {
+            if(selectedGroup == 'Arc 1'){
+                return colorScale1(i)
+            }
+            if(selectedGroup == 'Arc 2'){
+                return colorScale2(i)
+            }
+            if(selectedGroup == 'Arc 3'){
+                return colorScale3(i)
+            }
+            if(selectedGroup == 'Arc 4'){
+                return colorScale4(i)
+            }
+            if(selectedGroup == 'Arc 5'){
+                return colorScale5(i)
+            }
+        })
+        .attr("x", (d,i) => {
+            if(i == 0){
+                return xScale("Filler")
+            }
+            if(i == 1){
+                return xScale("Canon")
+            }
+        })
+        .attr("y", d => yScale(d))
+        .attr("width", d => xScale.bandwidth())
+        .attr("height", d => yScale(0) - yScale(d))
+  }
+  // When the button is changed, run the updateChart function
+  d3.select("#selectButton").on("change", function(d) {
+    // recover the option that has been chosen
+    var selectedOption = d3.select(this).property("value")
+    // run the updateChart function with this selected option
+    update(selectedOption)
+ // }
+  //)
+  }
+  )
+    }
+)
